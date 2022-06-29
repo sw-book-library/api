b2a5753a00bd74f288bdbaf4fad3efd6e9a90cdd
Oziel Silveira Junior <37221417+ozielsilveira@users.noreply.github.com>
2022-06-29 20:44:33 -0300

    adding adjustment to booking route

--- routes/reserve.js
+++ routes/reserve.js
@@ -3,7 +3,7 @@ var router = require("express").Router();
 const reserveController = require('../controllers/reserve');
 
 router.get("/", reserveController.findAll);
-router.get("/:id", reserveController.findOne);
+router.get("/:bookId&:userId", reserveController.findByUserOrBook);
 router.post("/", reserveController.create);
 router.put("/:id", reserveController.update);
 router.delete("/:id", reserveController.delete);
